7415376e4efb82c184310d64dd03ab96471b27bb
Pirat <sir34@ukr.net>
2020-09-26 13:46:03 +0300

    Lab 3

---
+++ C_third/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace C_third
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Введите число: ");
+            string number = Console.ReadLine();
+            int num1 = Convert.ToInt32(number);
+
+            if (num1 % 2 != 0)
+            {
+                Console.WriteLine("Это число непарное. ");
+            }
+            else
+            {
+                Console.WriteLine("Это число парное. ");
+            }
+
+            if (100 <= num1 && num1 < 1000)
+            {
+                Console.WriteLine("Это число есть трёхзначным! ");
+            }
+            else
+            {
+                Console.WriteLine("Это число не трёхзначное! ");
+            }
+        }
+    }
+}
