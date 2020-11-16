1f114f63ec3d2cedb9f8c71f22393351b7940a3f
Pirat <sir34@ukr.net>
2020-11-16 23:38:36 +0200

    Lab 7

---
+++ C_seven/C_seven/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace C_seven
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Введите сколько будет чисел: ");
+            int amount;
+
+            while (!int.TryParse(Console.ReadLine(), out amount) || amount <= 0)
+            {
+                Console.Write("Допускаются значения только больше 0. Попробуйте ещё раз: ");
+            }
+
+            List<int> list = new List<int>(amount);
+
+            for (int i = 0; i < list.Capacity; i++)
+            {
+                Console.Write($"Введите {i + 1} число: ");
+                int number;
+
+                while (!int.TryParse(Console.ReadLine(), out number) || number < 0 || number > 1)
+                {
+                    Console.Write("Вводите только 0 и 1: ");
+                }
+                list.Add(number);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Ваш список: ");
+
+                foreach (int element in list)
+                    Console.WriteLine(element);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Посчитаем количество 1 и 0: ");
+
+            int zeroCounter = 0;
+            int oneCounter = 0;
+
+            foreach (int element in list)
+                if (element == 0)
+                    zeroCounter++;
+                else oneCounter++;
+
+            Console.WriteLine();
+            Console.WriteLine($"Нули: {zeroCounter}");
+            Console.WriteLine($"Единицы: {oneCounter}");
+        }
+    }
+}
