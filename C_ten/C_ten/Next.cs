ca32416fe7c754435b50fe318389f52c9b6caf2e
Pirat <sir34@ukr.net>
2020-12-17 21:29:00 +0200

    Lab_10

---
+++ C_ten/C_ten/Next.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace C_ten
+{
+    public static class Nextt
+    {
+        public static int Reverse(this int znach)
+        {
+            int Rever = 0;
+            while (znach > 0)
+            {
+                int remainded = znach % 10;
+                Rever = (Rever * 10) + remainded;
+                znach = znach / 10;
+            }
+            return Rever;
+        }
+
+
+        public static string Reverse(this string znach)
+        {
+            string Rever = string.Empty;
+            for (int i = znach.Length - 1; i >= 0; i--)
+            {
+                Rever += znach[i];
+            }
+            return Rever;
+        }
+
+
+        public static string SplitReverse(this string znach)
+        {
+            string rever = string.Empty;
+            var strings = znach.Split(',');
+            for (int i = 0; i < strings.Length - 1; i++)
+            {
+                rever += Reverse(strings[i]) + ','; //tmp = rever+reverse(strings[i]) + ',';
+            }
+            rever += Reverse(strings[strings.Length - 1]);
+            return rever;
+        }
+
+
+        public static double SplitReverse(this double znach) =>
+    Double.Parse(SplitReverse(znach.ToString(CultureInfo.CurrentCulture)));
+
+        public static T[] Reverse<T>(this T[] array)
+        {
+            T[] newArray = new T[array.Length];
+            for (int i = 0, j = array.Length - 1; i < array.Length; i++, j--)
+                {
+                    newArray[i] = array[j];
+                }
+            return newArray;
+        }
+
+
+        public static void PrintArrayEvenOdd(this int[] array)
+        {
+            List<int> oddElements = array.Where(el => el % 2 == 0).ToList();
+            oddElements.ForEach(el => Console.Write(el + " "));
+            array.Except(oddElements).ToList().ForEach(el => Console.Write(el + " "));
+        }
+    }
+}
---
+++ C_ten/C_ten/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Globalization;
+using C_ten;
+
+namespace C_six
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            int Numbers = 123;
+            Console.WriteLine("Задание 1: \t" + Numbers.Reverse());
+            Console.WriteLine();
+
+            string Str = "Задание 2: Это тестовая строка ";
+            Console.Write(Str.Reverse());
+            Console.WriteLine();
+
+            double Fraction = 145.970;
+            Console.WriteLine("Задание 3: \t");
+            Console.Write(Convert.ToDouble(Fraction.SplitReverse()));
+            Console.WriteLine();
+
+            string Magic = "Opa (12) a chto (71) eto tyt y (50) nas";
+            Console.WriteLine("Задание 4: \t");
+            Console.Write(Magic.SplitReverse());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Задание 7: \t");
+            int[] RWR = new int[6] { 7, 4, 6, 9, 1, 0 };
+            var RevWithoutR = RWR.Reverse();
+            for (int i = 0; i < RevWithoutR.Length; i++)
+            {
+                Console.WriteLine("\t\t");
+                Console.Write(RevWithoutR[i]);
+            }
+            Console.WriteLine();
+
+            int[] var6 = { 5, 7, 1, 1, 0, 9 };
+            Console.Write("Задание варианта 6: ");
+            var6.PrintArrayEvenOdd();
+        }
+    }
+}
