56ceeaf0669b9f30c4ec72d68081bad3a10a9284
ninzamar <71394322+ninzamar@users.noreply.github.com>
2021-04-16 12:05:36 +0300

    Add files via upload

---
+++ C_lab_3/Lab3/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(344, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 0;
+            this.button1.TabStop = false;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(344, 228);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 400;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ C_lab_3/Lab3/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Point tmp_location;
+        Size tmp_size;
+        int counter = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("OK button has been pressed");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.X > button1.Location.X - button1.Size.Width / 2 && e.X < button1.Location.X + button1.Size.Width / 2 && e.Y > button1.Location.Y - button1.Size.Height / 2 && e.Y < button1.Location.Y + button1.Size.Height / 2)
+            {
+                tmp_location = button1.Location;
+                tmp_location.X += 10;
+                tmp_location.Y += 10;
+                button1.Location = new Point(tmp_location.X, tmp_location.Y);
+                tmp_size = button1.Size;
+                tmp_size.Width -= 1;
+                tmp_size.Height -= 1;
+                button1.Size = new Size(tmp_size.Width, tmp_size.Height);
+            }
+            if (e.X > button1.Location.X + button1.Size.Width / 2 && e.X < button1.Location.X + button1.Size.Width * 1.5 && e.Y > button1.Location.Y - button1.Size.Height / 2 && e.Y < button1.Location.Y + button1.Size.Height / 2)
+            {
+                tmp_location = button1.Location;
+                tmp_location.X -= 10;
+                tmp_location.Y += 10;
+                button1.Location = new Point(tmp_location.X, tmp_location.Y);
+                tmp_size = button1.Size;
+                tmp_size.Width -= 1;
+                tmp_size.Height -= 1;
+                button1.Size = new Size(tmp_size.Width, tmp_size.Height);
+            }
+            if (e.X > button1.Location.X - button1.Size.Width / 2 && e.X < button1.Location.X + button1.Size.Width / 2 && e.Y > button1.Location.Y + button1.Size.Height / 2 && e.Y < button1.Location.Y + button1.Size.Height * 1.5)
+            {
+                tmp_location = button1.Location;
+                tmp_location.X += 10;
+                tmp_location.Y -= 10;
+                button1.Location = new Point(tmp_location.X, tmp_location.Y);
+                tmp_size = button1.Size;
+                tmp_size.Width -= 1;
+                tmp_size.Height -= 1;
+                button1.Size = new Size(tmp_size.Width, tmp_size.Height);
+            }
+            if (e.X > button1.Location.X + button1.Size.Width / 2 && e.X < button1.Location.X + button1.Size.Width * 1.5 && e.Y > button1.Location.Y + button1.Size.Height / 2 && e.Y < button1.Location.Y + button1.Size.Height * 1.5)
+            {
+                tmp_location = button1.Location;
+                tmp_location.X -= 10;
+                tmp_location.Y -= 10;
+                button1.Location = new Point(tmp_location.X, tmp_location.Y);
+                tmp_size = button1.Size;
+                tmp_size.Width -= 1;
+                tmp_size.Height -= 1;
+                button1.Size = new Size(tmp_size.Width, tmp_size.Height);
+            }
+            if (tmp_size.Height == 0 && tmp_size.Width == 0)
+            {
+                timer3.Start();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Press 'OK'!!!";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Text = "Press 'OK'!!!";
+            counter++;
+            timer2.Start();
+            if (counter == 8)
+            {
+                timer1.Stop();
+                timer2.Stop();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.Text = " ";
+            timer2.Stop();
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            this.Text = "'OK' cannot be pressed!!!";
+            timer2.Start();
+        }
+    }
+}
