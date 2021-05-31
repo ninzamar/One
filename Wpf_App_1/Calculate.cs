7125329126898b2cc0441d47ea5d59f6f0f93724
ninzamar <71394322+ninzamar@users.noreply.github.com>
2021-05-31 14:58:34 +0300

    Add files via upload

---
+++ Wpf_App_1/Calculate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Calculate
+    {
+        public double add(double a, double b)
+        {
+            return (a + b);
+        }
+
+        public double subtract(double a, double b)
+        {
+            return (a - b);
+        }
+
+        public double multiply(double a, double b)
+        {
+            return (a * b);
+        }
+
+        public double divide(double a, double b)
+        {
+            return (a / b);
+        }
+    }
+}
